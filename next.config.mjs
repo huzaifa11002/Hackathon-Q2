947311da5da1169184e49d4929f83157dd94b9f4
huzaifa11002 <huzaifahuzaifa112@gmail.com>
2025-01-21 19:26:36 +0500

    some changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,4 @@
 /** @type {import('next').NextConfig} */
-import withBundleAnalyzer from '@next/bundle-analyzer';
 const nextConfig = {
   images: {
     remotePatterns: [
@@ -12,6 +11,4 @@ const nextConfig = {
   },
 };
 
-export default withBundleAnalyzer({
-  enabled: process.env.ANALYZE === 'true',
-})(nextConfig);
+export default nextConfig;
